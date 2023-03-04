ecd3f46c7ba9122af0da62e1dc03c264f1c37cb2
Youhoseong <h0song@naver.com>
2023-03-04 19:13:21 +0900

    Configure event executor for async

--- src/main/kotlin/com/example/eventbus/spring/SpringEventConfig.kt
+++ src/main/kotlin/com/example/eventbus/spring/SpringEventConfig.kt
@@ -2,9 +2,13 @@ package com.example.eventbus.spring
 
 import com.example.eventbus.spring.publisher.SpringEventPublisher
 import com.example.eventbus.spring.subscriber.SpringEventSubscriber
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ApplicationEventMulticaster
+import org.springframework.context.event.SimpleApplicationEventMulticaster
+import org.springframework.core.task.SimpleAsyncTaskExecutor
 
 @Configuration
 class SpringEventConfig {
@@ -17,5 +21,13 @@ class SpringEventConfig {
     @Bean(name = ["springEventPublisher"])
     fun eventPublisher(applicationEventPublisher: ApplicationEventPublisher): SpringEventPublisher {
         return SpringEventPublisher(applicationEventPublisher = applicationEventPublisher)
+    }
+
+    @Bean(name = ["applicationEventMulticaster"]) // bean name must be fixed
+    @ConditionalOnMissingBean
+    fun applicationEventMulticaster(): ApplicationEventMulticaster {
+        val eventMulticaster = SimpleApplicationEventMulticaster()
+        eventMulticaster.setTaskExecutor(SimpleAsyncTaskExecutor())
+        return eventMulticaster
     }
 }
